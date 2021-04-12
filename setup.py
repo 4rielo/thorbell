d58a46259ec94b02ca9358c6034c384054d813ed
4rielo <ascarafia@gmail.com>
2021-04-12 14:58:27 -0300

    Now we're on version 0.0.8, added icons

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 #setup.py
-#from setuptools import setup
-from distutils.core import setup
+from setuptools import setup
+#from distutils.core import setup
 
 # This call to setup() does all the work
 setup (
     name = 'thorbell',
-    version = '0.0.7',
+    version = '0.0.8',
     description="Firmware for CSB-MercurioR1, from THORBELL",
     url="https://github.com/4rielo/thorbell.git",
     author="Ariel Scarafia",
     author_email="ombas.gm@gmail.com",
-    packages=['CSB_MercurioR1','data','docs','fonts','icons','images'],
+    packages=['CSB_MercurioR1'],
     include_package_data=True
 )
