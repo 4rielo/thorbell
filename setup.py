2159f6cbc9d931c1188ce472c35e484285c1000f
4rielo <ascarafia@gmail.com>
2021-04-12 10:41:55 -0300

    Now we're on version 0.0.4 :/ WTF. Would this time work?

--- setup.py
+++ setup.py
@@ -2,12 +2,6 @@
 import pathlib
 from setuptools import setup
 
-# The directory containing this file
-HERE = pathlib.Path(__file__).parent
-
-# The text of the README file
-README = (HERE / "README.md").read_text
-
 include_files = ['*.txt']
 
 # This call to setup() does all the work
@@ -15,8 +9,6 @@ setup (
     name = 'thorbell',
     version = '0.0.4',
     description="Firmware for CSB-MercurioR1, from THORBELL",
-    long_description=README,
-    long_description_content_type="text/markdown",
     url="https://github.com/4rielo/thorbell.git",
     author="Ariel Scarafia",
     author_email="ombas.gm@gmail.com",
