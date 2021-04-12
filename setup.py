06e84d9e6d40a43339891d2dcc8d824b816f1c3c
4rielo <ascarafia@gmail.com>
2021-04-12 17:52:47 -0300

    v0.0.11, added fonts, and images folders

--- setup.py
+++ setup.py
@@ -13,11 +13,7 @@ setup (
     packages=['CSB_MercurioR1'],
     package_dir={'CSB_MercurioR1':'CSB_MercurioR1'},
     package_data={
-        'CSB_MercurioR1': ['data/*'],
-        'CSB_MercurioR1': ['docs/*'],
-        'CSB_MercurioR1': ['fonts/*'],
-        'CSB_MercurioR1': ['icons/*.png'],
-        'CSB_MercurioR1': ['images/*.png'],
+        'CSB_MercurioR1': ['data/*', 'docs/*','fonts/*','icons/*.png','images/*.png'],
         },
     include_package_data=True,
 )
