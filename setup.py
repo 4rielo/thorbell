5c47ded4e3ced3b5fbce237612af885a49856289
4rielo <ascarafia@gmail.com>
2021-04-12 15:27:56 -0300

    v0.0.9, added data, docs, fonts, and images folders

--- setup.py
+++ setup.py
@@ -5,12 +5,11 @@ from setuptools import setup
 # This call to setup() does all the work
 setup (
     name = 'thorbell',
-    version = '0.0.8',
+    version = '0.0.9',
     description="Firmware for CSB-MercurioR1, from THORBELL",
     url="https://github.com/4rielo/thorbell.git",
     author="Ariel Scarafia",
     author_email="ombas.gm@gmail.com",
     packages=['CSB_MercurioR1'],
     include_package_data=True,
-    package_data={"":['*.*'],},
 )
