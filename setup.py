2104d0c426e5f4450f6fee1490db4b6a9972fedb
4rielo <ascarafia@gmail.com>
2021-04-12 14:45:30 -0300

    Now we're on version 0.0.7, I think I got it to work now!

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 # This call to setup() does all the work
 setup (
     name = 'thorbell',
-    version = '0.0.6',
+    version = '0.0.7',
     description="Firmware for CSB-MercurioR1, from THORBELL",
     url="https://github.com/4rielo/thorbell.git",
     author="Ariel Scarafia",
